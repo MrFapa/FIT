c9d475974ed12ff52cb706425740adc495c9ba53
MrFapa <s.scholzfabian@gmail.com>
2021-12-09 12:48:50 +0100

    Init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float maxForce;
+    [SerializeField]
+    private float maxSpeed;
+    [SerializeField]
+    private float maxVelocity;
+
+    private Vector3 target;
+
+    private Vector3 vel;
+
+    private SeekerBehaviour sb;
+    
+
+    void Start()
+    {
+        this.vel = new Vector3(0, 0, 0);
+        sb = new SeekerBehaviour();
+    }
+
+    void Update()
+    {
+        this.target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        this.target.z = 0;
+
+
+        Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.vel = sb.calcSeekingVector(target, this.transform.position, vel, maxSpeed, maxForce, maxVelocity);
+        move();
+    }
+
+    void move()
+    {
+        this.transform.position += this.vel;
+    }
+
+    void edgeCheck()
+    {
+        
+
+    }
+}
---
+++ Assets/Scripts/SeekerBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeekerBehaviour 
+{
+    public Vector3 calcSeekingVector(Vector3 target, Vector3 currentPos, Vector3 vel, float maxSpeed, float maxForce, float maxVelocity)
+    {
+        Vector3 desired = target - currentPos;
+        desired = desired.normalized * maxSpeed;
+
+        Vector3 steering = desired - vel;
+        steering = Vector3.ClampMagnitude(steering, maxForce);
+
+        Vector3 newVel = Vector3.ClampMagnitude(vel + steering, maxSpeed);
+        return newVel;
+    }
+}
